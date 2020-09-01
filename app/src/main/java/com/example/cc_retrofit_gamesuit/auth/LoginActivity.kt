c907e497841df8428b4f3ba4e288db81eb534966
Fritzent <afritzent@gmail.com>
2020-09-01 13:31:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cc_retrofit_gamesuit/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cc_retrofit_gamesuit.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cc_retrofit_gamesuit.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
