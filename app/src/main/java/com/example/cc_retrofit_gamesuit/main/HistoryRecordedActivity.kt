81a967782fbc7d24d9bc64d8f006e4f581e21802
primsaphira <primsaphira@gmail.com>
2020-09-03 13:04:03 +0700

    history game

--- app/src/main/java/com/example/cc_retrofit_gamesuit/main/HistoryRecordedActivity.kt
+++ app/src/main/java/com/example/cc_retrofit_gamesuit/main/HistoryRecordedActivity.kt
@@ -8,5 +8,6 @@ class HistoryRecordedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history_recorded)
+
     }
 }
