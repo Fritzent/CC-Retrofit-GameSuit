6decd8ecfd211a089d42dbdc8d4e47935bb29012
andratitano <andrabusra10@gmail.com>
2020-09-03 13:31:45 +0700

    profil dan halaman home

---
+++ app/src/main/java/com/example/cc_retrofit_gamesuit/home/HomeActivity.kt
@@ -0,0 +1,59 @@
+package com.example.cc_retrofit_gamesuit.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.example.cc_retrofit_gamesuit.R
+import com.example.cc_retrofit_gamesuit.databinding.ActivityHomeBinding
+import com.example.cc_retrofit_gamesuit.profil.ProfilFragment
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val botNav = binding.bottomNavigation
+
+        try {
+            this.supportActionBar?.hide()
+        } catch (e: NullPointerException) {}
+
+        val homeFragment = HomeFragment()
+        val profilFragment = ProfilFragment()
+
+        //default fragment
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameHomecontainer, homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+
+        botNav.setOnNavigationItemSelectedListener {
+            return@setOnNavigationItemSelectedListener when (it.itemId) {
+                R.id.navBeranda -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frameHomecontainer, homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                    true
+                }
+
+                R.id.navTest -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frameHomecontainer, profilFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cc_retrofit_gamesuit/profil/ProfilActivity.kt
@@ -0,0 +1,24 @@
+package com.example.cc_retrofit_gamesuit.profil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cc_retrofit_gamesuit.R
+import kotlinx.android.synthetic.main.fragment_edit_email.*
+import kotlinx.android.synthetic.main.fragment_edit_username.*
+
+class ProfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+
+        tvUsernameEdit.setOnClickListener {
+            val fm  = supportFragmentManager
+            EditUsernameFragment().show(fm,"Edit Username Fragment")
+        }
+
+        tvEmailEdit.setOnClickListener {
+            val fm  = supportFragmentManager
+            EditEmailFragment().show(fm,"Edit Email Fragment")
+        }
+    }
+}
