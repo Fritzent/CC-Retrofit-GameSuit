9f2502e178fdd31972a2295e155df9f1e4032b02
primsaphira <primsaphira@gmail.com>
2020-09-03 12:05:15 +0700

    history game

--- app/src/main/java/com/example/cc_retrofit_gamesuit/main/HistoryGameActivity.kt
+++ app/src/main/java/com/example/cc_retrofit_gamesuit/main/HistoryGameActivity.kt
@@ -1,40 +1,33 @@
 package com.example.cc_retrofit_gamesuit.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.cc_retrofit_gamesuit.R
+import kotlinx.android.synthetic.main.activity_history_game.*
 
 class HistoryGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history_game)
+
+        Toast.makeText(this,"Data Masih Kosong", Toast.LENGTH_LONG)
+
+        btnBattle.setOnClickListener {
+            val intent = Intent(this, HistoryRecordedActivity::class.java)
+            startActivity(intent)
+
+        }
+        btnHistory.setOnClickListener {
+            val intent = Intent(this, HistoryRecordedActivity::class.java)
+            startActivity(intent)
+
+        }
+        btnProfile.setOnClickListener {
+            val intent = Intent(this, HistoryRecordedActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
-
-
-//TODO INI BAGIAN YANG ERROR DI XML activity_history_game
-//<ImageButton
-//android:id="@+id/btnBattle"
-//android:layout_width="wrap_content"
-//android:layout_height="wrap_content"
-//app:layout_constraintBottom_toBottomOf="parent"
-//app:layout_constraintStart_toStartOf="parent"
-//android:src="@drawable/ic_battle"/>
-//
-//<ImageButton
-//android:id="@+id/btnHistory"
-//android:layout_width="wrap_content"
-//android:layout_height="wrap_content"
-//android:layout_marginBottom="16dp"
-//app:layout_constraintBottom_toBottomOf="parent"
-//app:layout_constraintEnd_toStartOf="@id/btnProfile"
-//app:layout_constraintStart_toEndOf="@id/btnBattle"
-//android:src="@drawable/ic_history"/>
-//
-//<ImageButton
-//android:id="@+id/btnProfile"
-//android:layout_width="wrap_content"
-//android:layout_height="wrap_content"
-//app:layout_constraintBottom_toBottomOf="parent"
-//app:layout_constraintEnd_toEndOf="parent"
-//android:src="@drawable/ic_profile"/>
---
+++ app/src/main/java/com/example/cc_retrofit_gamesuit/main/HistoryRecordedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cc_retrofit_gamesuit.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cc_retrofit_gamesuit.R
+
+class HistoryRecordedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_recorded)
+    }
+}
